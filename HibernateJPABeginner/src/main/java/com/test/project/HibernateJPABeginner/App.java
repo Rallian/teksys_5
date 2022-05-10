0e89fbc203b80a3ea133118bad03eff7e0444de4
Travis <travismollohan44@gmail.com>
2022-05-10 10:13:09 -0700

    html practice day 1

---
+++ HibernateJPABeginner/src/main/java/com/test/project/HibernateJPABeginner/App.java
@@ -0,0 +1,18 @@
+package com.test.project.HibernateJPABeginner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import com.test.hib.controller.findUser_Hql;
+import com.test.hib.model.User;
+
+public class App {
+	
+	public static void main( String[] args )
+    {
+      findUser_Hql u = new findUser_Hql();
+    	u.getRecordbyId();
+    }
+
+}
